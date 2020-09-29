a1fc0c62b04db097fa2e7881bb655d87dabe18eb
Nayar Joolfoo <nayar@joolfoo.com>
2020-09-29 16:15:39 +0400

    remove simple items

--- cvparser.py
+++ cvparser.py
@@ -5,8 +5,6 @@ from tika import parser
 cvs = []
 for (dirpath, dirnames, filenames) in walk('./cv/'):
     cvs.extend(filenames)
-    
-items = ['linux','cloud','container','docker','terraform','git']
 
 results = {
     'linux'         : ['linux','debian','centos','rhce','ubuntu'],
@@ -26,10 +24,6 @@ for cv in cvs:
         cnt = cnt.split()
         counts = Counter(cnt)
         
-        line = ""
-        for item in items:
-            line = line + str(counts[item]) + ','
-        
         output = ""
         total = 0
         for result in results:
@@ -40,7 +34,6 @@ for cv in cvs:
                     total += res
                     break
             output += str(res) + ','
-        #data += "%s %s %i\t\t\t%s\n" % (line,output,total,cv)
         data += "%s %i\t\t\t%s\n" % (output,total,cv)
         
 f = open("./data.txt", "w")
