0ef692e873500d5326dfc32a545af63dfd52797f
Nayar Joolfoo <nayar@joolfoo.com>
2020-09-29 12:08:00 +0400

    v1

--- cvparser.py
+++ cvparser.py
@@ -1,20 +1,48 @@
 from collections import Counter
-import tokenize
 from os import walk
 from tika import parser
 
 cvs = []
 for (dirpath, dirnames, filenames) in walk('./cv/'):
     cvs.extend(filenames)
+    
+items = ['linux','cloud','container','docker','terraform','git']
 
+results = {
+    'linux'         : ['linux','debian','centos','rhce'],
+    'cloud'         : ['cloud','aws','azure','gcp'],
+    'git'           : ['git'],
+    'container'     : ['container','docker','lxc'],
+    'cicd'          : ['cicd','gitlab','automate','terraform','ansible','automation'],
+    'monitoring'    : ['monitoring'],
+    'documentation' : ['documentation']
+}
+
+data = ""
 for cv in cvs:
         cv = './cv/' + cv
         raw = parser.from_file(cv)
-        cnt = str(raw['content']).lower()
+        cnt = str(raw['content']).lower().replace(",",'').replace(".",'').replace("/",' ')
         cnt = cnt.split()
         counts = Counter(cnt)
-        print(counts['linux'])
         
-        f = open("./tmp/" + cv + '.input.txt', "a")
-        f.write(str(counts))
-        f.close()
+        line = ""
+        for item in items:
+            line = line + str(counts[item]) + ','
+        
+        output = ""
+        total = 0
+        for result in results:
+            res = 0
+            for i in results[result]:
+                if(counts[i] > 0):
+                    res = 1
+                    total += res
+                    break
+            output += str(res) + ','
+        data += "%s %s %i\t\t\t%s\n" % (line,output,total,cv)
+        
+f = open("./data.txt", "w")
+f.write(data)
+print(data)
+f.close()
